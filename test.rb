c0cb20f9effe48de8e59a9aaee3919978a62d7df
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-14 18:32:27 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,9 @@
+def base_hash
+  # Remember implicit return! We're returning this new Hash _without_ the
+  # keyword "return." Nice and neat.
+  {:railroads => {}}
+
+end
+
+new_hash = ...base_hash
+puts new_hash
